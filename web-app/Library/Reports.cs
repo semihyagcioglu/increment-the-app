8cfc2cdba7bb8b14b3eed49766f175d62dad5c3a
Salih KARAHAN <contact@salihkarahan.com>
2013-12-02 10:54:55 +0200

    Add a Reports.cs
    Add a Reports.cs and create a method

---
+++ web-app/Library/Reports.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace increment_the_app.Library
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class Reports //
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="interval"></param>
+        public void GetRegistrationCountByInterval(string interval)
+        {
+
+        }
+    }
+}
