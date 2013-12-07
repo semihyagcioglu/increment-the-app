2566e64fc9ca1362eb97e23f39b4b8b6f604539c
Semih Yağcıoğlu <semihyagcioglu@yahoo.com>
2013-12-07 21:34:08 +0200

    Converted pages to work with MasterPage.

--- web-app/SignUp.aspx.designer.cs
+++ web-app/SignUp.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace increment_the_app {
     
     
     public partial class SignUp {
-        
-        /// <summary>
-        /// CheckBox1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox CheckBox1;
     }
 }
