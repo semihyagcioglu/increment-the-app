8482c86c2de35a17d7da7ad1418147d1a483d6c2
Egemen Uluçay <egemen.ulucay@windowslive.com>
2014-02-18 21:14:26 +0200

    Added SignOut

--- web-app/Site.Master.designer.cs
+++ web-app/Site.Master.designer.cs
@@ -103,6 +103,15 @@ namespace increment_the_app {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl btnSignUp;
         
         /// <summary>
+        /// btnLogOut control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl btnLogOut;
+        
+        /// <summary>
         /// bodyContent control.
         /// </summary>
         /// <remarks>
