f3d7c812f20f367bd3ea051161afdb019302a69f
fatihakgul89 <fatihakgul89@yahoo.com>
2014-02-19 21:37:41 +0200

    GetLastTask Method Update

--- web-app/Library/Tasks.cs
+++ web-app/Library/Tasks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,27 @@ namespace increment_the_app.Library
 {
     public class Tasks
     {
+        private DataTable GetLastTask(int count)
+        {
+            string sql = @"SELECT TOP (" + count + @")[ID]
+                                  ,[UserID] AS [Kullanıcı No]
+                                  ,[TaskTitle] AS [İş]
+                                  ,[TaskDetail] AS [İşin Detayı]
+                                  ,[Date] AS [Tarih]
+                                  ,[Location] AS [Yapılacak Yer]
+                                  ,[Money] AS [İşin Ücreti]
+                                  ,[TaskStatus] AS [İşin Durum]
+                              FROM [Tasks]
+                                   WHERE [TaskStatus] = '1' ORDER BY DESC";
+
+            DataTable dtTasks = Library.DataBase.GetDataTable(sql);
+
+            return dtTasks;
+
+
+
+        }
     }
+    
+
 }
