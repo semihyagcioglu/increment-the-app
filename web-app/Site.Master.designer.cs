ed34e12dfebb23ce7d301f8f3c3f94b87c30cb15
ALi ÖZTÜRK <ali.ozturk0638@gmail.com>
2014-02-19 20:04:49 +0200

    MasterPage Update

--- web-app/Site.Master.designer.cs
+++ web-app/Site.Master.designer.cs
@@ -67,6 +67,15 @@ namespace increment_the_app {
         protected global::System.Web.UI.HtmlControls.HtmlInputHidden hdnSessionId;
         
         /// <summary>
+        /// hdnUsername control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputHidden hdnUsername;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
