cdc9e35fae9d4d4a7ebcc989d63fd5173308e355
Recep Sungurtaş <sungurtas.recep@gmail.com>
2014-02-19 21:20:57 +0200

    reset pasword
    edit LogIn.aspx and added ResetPassword.aspx

--- web-app/ResetPassword.aspx.designer.cs
+++ web-app/ResetPassword.aspx.designer.cs
@@ -10,15 +10,24 @@
 namespace increment_the_app {
     
     
-    public partial class LogIn {
+    public partial class ResetPassword {
         
         /// <summary>
-        /// hypResetPass control.
+        /// lblEmail control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink hypResetPass;
+        protected global::System.Web.UI.WebControls.Label lblEmail;
+        
+        /// <summary>
+        /// txtEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
     }
 }
