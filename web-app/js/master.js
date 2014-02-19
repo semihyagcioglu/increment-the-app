ae6feab9cb97b54e38aa9159d789cbe449433afb
ALi ÖZTÜRK <ali.ozturk0638@gmail.com>
2014-02-20 00:00:21 +0200

    Master.js ,NonSessionSite and Logs.cs Updated

--- web-app/js/master.js
+++ web-app/js/master.js
@@ -10,7 +10,7 @@ $(window).load(function() {
     $.ajax({
         type: "POST",
         url: "WsUsers.asmx/UpdatePageIn",
-        data: "{" + "'userId':'" + $("#ctl00_hdnUserId").val() + "','page':'" + document.URL + "','ip':'" + $('#ctl00_hdnUserIpAdress').val() + "','uniqueId':'" + $('#ctl00_hdnSessionId').val() + "'}",
+        data: "{" + "'userId':'" + $("#hdnUserId").val() + "','page':'" + document.URL + "','ip':'" + $('#hdnUserIpAdress').val() + "','uniqueId':'" + $('#hdnSessionId').val() + "'}",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(msg) {
@@ -31,7 +31,7 @@ $(window).unload(function() {
         type: "POST",
         async: false,
         url: "../WsUsers.asmx/UpdatePageOut",
-        data: "{" + "'userId':'" + $("#ctl00_hdnUserId").val() + "','uniqueId':'" + $('#ctl00_hdnSessionId').val() + "'}",
+        data: "{" + "'userId':'" + $("#hdnUserId").val() + "','uniqueId':'" + $('#hdnSessionId').val() + "'}",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(msg) {
