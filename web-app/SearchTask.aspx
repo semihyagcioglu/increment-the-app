0f7fd599e29100f4c3bbd6998b2d78de88a97ab2
Egemen Uluçay <egemen.ulucay@windowslive.com>
2014-02-24 21:41:53 +0200

    Updated searchTask.js

--- web-app/SearchTask.aspx
+++ web-app/SearchTask.aspx
@@ -1,5 +1,6 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SearchTask.aspx.cs" Inherits="increment_the_app.WebForm2" %>
 <asp:Content ID="hc" ContentPlaceHolderID="headContent" runat="server">
+    <script src="js/searchTasks.js"></script>
 </asp:Content>
 <asp:Content ID="bc" ContentPlaceHolderID="bodyContent" runat="server">
 
@@ -9,8 +10,8 @@
     <div>
     <form class="navbar-form navbar-left" role="search">
   <div class="form-group">
-    <input type="text" class="form-control" style="width:500px; margin-right:10px; float:left;" placeholder="Aranacak anahtar kelimeyi giriniz...">
-      <button type="submit" style="width:100px;" class="btn btn-success">Ara</button>
+    <input type="text" class="form-control" id="InputSearchTask" style="width:500px; margin-right:10px; float:left;" placeholder="Aranacak anahtar kelimeyi giriniz...">
+      <button type="button" style="width:100px;" id="btnSearchTask" class="btn btn-success">Ara</button>
   </div>  
 </form>
 
---
+++ web-app/js/searchTasks.js
@@ -0,0 +1,58 @@
+﻿function   SearchTask(task) {
+
+    var validated = true;
+    var warningText = '';
+
+    if (IsNullOrEmpty(task) == true) {
+
+        $('#InputSearchTask').closest('.form-group').addClass("has-error");
+        $('#InputSearchTask').focus();
+
+        warningText = "Lütfen Yapılmak İstenen İş Giriniz...";
+        validated = false;
+    }
+    else {
+
+        $.ajax({
+            type: "POST",
+            url: "WsTasks.asmx/SearchTask",
+            data: "{" + "'searchTask':'" + task + "'}",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (msg) {
+
+           
+                    //Success
+                    window.location.href = 'Home.aspx';
+
+                
+
+            },
+            error: function () {
+                //some error occured
+                $("#warningMessage").text('Kayıt sırasında beklenmeyen bir hata oluştu.');
+                $("#warningMessage").show();
+            }
+        });
+    }
+}
+
+$(document).ready(function () {
+
+    $('#InputSearchTask').focus();
+
+    //enter key event
+    $("body").keypress(function (e) {
+        if (e.which == 13) {
+            $("#btnSearchTask").click();
+        }
+    });
+
+    $("#btnSearchTask").click(function () {
+
+        var task = $("#InputSearchTask").val();       
+
+        SearchTask(task);
+    });
+
+});
