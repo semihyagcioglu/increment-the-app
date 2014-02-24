0faa0e7bafdeb2a1181d3f8df48258ce625d8d07
Egemen Uluçay <egemen.ulucay@windowslive.com>
2014-02-24 20:47:20 +0200

    Added WsTasks.asmx

---
+++ web-app/WsTasks.asmx.cs
@@ -0,0 +1,28 @@
+﻿using increment_the_app.Library;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace increment_the_app
+{
+    /// <summary>
+    /// Summary description for WsTasks
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WsTasks : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public DataTable SearchTask(string task)
+        {
+            return Tasks.SearchTask(task);
+        }
+    }
+}
