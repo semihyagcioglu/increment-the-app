5993cabe38aa202abb096591beda3b9ae1f40820
Egemen Uluçay <egemen.ulucay@windowslive.com>
2014-02-26 21:36:46 +0200

    updated Ws.SearchTask

--- web-app/WsTasks.asmx.cs
+++ web-app/WsTasks.asmx.cs
@@ -15,14 +15,14 @@ namespace increment_the_app
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+    [System.Web.Script.Services.ScriptService]
     public class WsTasks : System.Web.Services.WebService
     {
 
         [WebMethod]
-        public List<DataRow> SearchTask(string task)
+        public List<DataRow> SearchTask(string searchTask)
         {
-            return Tasks.SearchTask(task);
+            return Tasks.SearchTask(searchTask);
         }
 
     }
