209b71237739fc6701a43dfd0a80027b2fac9f83
Egemen Uluçay <egemen.ulucay@windowslive.com>
2014-02-27 20:32:19 +0200

    updated profile details

--- web-app/Profile.aspx.cs
+++ web-app/Profile.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +12,32 @@ namespace increment_the_app
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                ProfileDetails();
+            }
+        }
+
+        private void ProfileDetails()
+        {
+            int userID = int.Parse(Session["UserId"].ToString());
+            string selectUser = @"Select [dbo].[Users]
+                                   SET [Name] = @Name
+                                      ,[Surname] = @Surname                                     
+                                      ,[Email] = @Email
+                                      ,[Gender] = @Gender
+                                      ,[BirthDate] = @BirthDate                                  
+                                      ,[GSM] = @Gsm                                     
+                                 WHERE UserId= @UserId";
+
+            DataTable userProfile = Library.DataBase.GetDataTable(selectUser);
+
+            InputName.Value = userProfile.Rows[0]["Name"].ToString();
+            InputSurname.Value = "";
+            InputEmail.Value = "";
+            InputPhone.Value = "";
+            InputAdress.Value = "";
+
 
         }
     }
