3990f53fb59c9a20d516a1f294d0887f84fc65ef
Mehmet Zahir <m.zahirr@hotmail.com>
2014-03-04 19:52:49 +0200

    ResetPassword.aspx updated

--- web-app/ResetPassword.aspx.designer.cs
+++ web-app/ResetPassword.aspx.designer.cs
@@ -13,30 +13,12 @@ namespace increment_the_app {
     public partial class ResetPassword {
         
         /// <summary>
-        /// lblEmail control.
+        /// OldPassword control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblEmail;
-        
-        /// <summary>
-        /// txtEmail control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtEmail;
-        
-        /// <summary>
-        /// btnSendPassword control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSendPassword;
+        protected global::System.Web.UI.HtmlControls.HtmlInputPassword OldPassword;
     }
 }
