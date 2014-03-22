b0a10e161d0411575965d1fe9a633f9a711a69f8
ALi ÖZTÜRK <ali.ozturk0638@gmail.com>
2014-03-22 11:29:57 +0200

    Update

--- web-app/js/searchTasks.js
+++ web-app/js/searchTasks.js
@@ -106,7 +106,7 @@
                 else {
                     if (index === 'UserID') {
                        
-                        str += '<hr><div class="result-avatar" style="float:left; margin-right:10px; margin-left:15px; "><img src="Image.ashx?id=' + array[i]['UserId'] + '" id="imgProfile" class="img-rounded" style="width:50px; height:50px;"> </div>';
+                        str += '<hr><div class="result-avatar" style="float:left; margin-right:10px; margin-left:15px; "><img src="Image.ashx?id=' + array[i]['UserID'] + '" id="imgProfile" class="img-rounded" style="width:50px; height:50px;"> </div>';
                     }
 
 
