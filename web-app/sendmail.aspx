26a1c0dbaa67b9da57f8eebf0f961aec4507af70
Recep Sungurtaş <sungurtas.recep@gmail.com>
2014-03-30 21:30:01 +0300

    test Send mAİL

---
+++ web-app/sendmail.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="sendmail.aspx.cs" Inherits="increment_the_app.sendmail" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:button runat="server" text="Button" OnClick="Unnamed1_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ web-app/sendmail.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Mail;
+
+using SendGridMail;
+using SendGrid.Transport;
+
+namespace increment_the_app
+{
+    public partial class sendmail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            MailMessage mailMsg = new MailMessage();
+
+            // To
+            mailMsg.To.Add(new MailAddress("info@increment.com", "increment site"));
+
+            // From
+            mailMsg.From = new MailAddress("sungurtas.recep@gmail.com", "recep sungurtas");
+
+            // Subject and multipart/alternative Body
+            mailMsg.Subject = "subject";
+            string text = "text body";
+            string html = @"<p>html body</p>";
+
+
+            // Init SmtpClient and send
+            SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("azure_093dfa8238abf9d9703183d4a7c559f0@azure.com", "x4f0Zq9XO7Rj6JQ");
+            smtpClient.Credentials = credentials;
+
+            smtpClient.Send(mailMsg);
+
+        }
+    }
+}
