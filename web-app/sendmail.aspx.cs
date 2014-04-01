d84d1027481069aa623dfebcc4067baf3e34fc15
Recep Sungurtaş <sungurtas.recep@gmail.com>
2014-04-01 21:14:38 +0300

    test 3 send mail

--- web-app/sendmail.aspx.cs
+++ web-app/sendmail.aspx.cs
@@ -16,31 +16,25 @@ namespace increment_the_app
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         protected void Unnamed1_Click(object sender, EventArgs e)
         {
-            MailMessage mailMsg = new MailMessage();
+            
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.sendgrid.net");
 
-            // To
-            mailMsg.To.Add(new MailAddress("info@increment.com", "increment site"));
+            mail.From = new MailAddress("increment@increment.com");
+            mail.To.Add("sungurtas.recep@gmail.com");
+            mail.Subject = "deneme";
+            mail.Body = "Report";
+       
+            SmtpServer.Port = 25;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("azure_093dfa8238abf9d9703183d4a7c559f0@azure.com", "x4f0Zq9XO7Rj6JQ");
+            SmtpServer.EnableSsl = true;
 
-            // From
-            mailMsg.From = new MailAddress("sungurtas.recep@gmail.com", "recep sungurtas");
-
-            // Subject and multipart/alternative Body
-            mailMsg.Subject = "subject";
-            string text = "text body";
-            string html = @"<p>html body</p>";
-
-
-            // Init SmtpClient and send
-            SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
-            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("azure_093dfa8238abf9d9703183d4a7c559f0@azure.com", "x4f0Zq9XO7Rj6JQ");
-            smtpClient.Credentials = credentials;
-
-            smtpClient.Send(mailMsg);
+            SmtpServer.Send(mail);
 
         }
     }
